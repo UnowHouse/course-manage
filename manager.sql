26925a0665ca88f15621092b366c480fa02388fd
UnowHouse <965545403@qq.com>
2018-12-18 00:17:02 +0800

    I'm push the sql file of manager table

---
+++ manager.sql
@@ -0,0 +1,13 @@
+/*
+SQLyog Ultimate v12.5.0 (64 bit)
+MySQL - 5.1.37-community 
+*********************************************************************
+*/
+/*!40101 SET NAMES utf8 */;
+
+create table `manager` (
+	`id` bigint (20),
+	`username` varchar (96),
+	`password` varchar (96),
+	`sickname` varchar (96)
+); 
